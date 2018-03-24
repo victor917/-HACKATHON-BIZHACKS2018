43d3ef546fa3a13c321271749cceb89b17a7c451
Luc Murakami <luc.murakami@hotmail.com>
2018-03-23 20:54:26 -0700

    added basic structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      choice : ''
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <nav >
+        <a href='#'>Home</a>
+        <a href='#'>About Us</a>
+        <a href='#'>Contacts</a>
+      </nav>
+
+      
+      
     );
   }
 }
---
+++ my-app/src/Components/BuildPart1.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+/* make your own CSS file and import it here */
+
+class BuildPart1 extends Component {
+  constructor() {
+    super()
+    this.state = {
+      choice : ''
+    }
+  }
+  render() {
+    return (
+      <div>
+
+
+      
+      </div>
+    );
+  }
+}
+
+export default BuildPart1;
